627e44077b41eb1e5027ea627dc90948470e48e8
Rhuan Pablo Monteiro Da Silva <rhuanpablomonteiro@gmail.com>
2024-07-01 15:47:38 -0300

    feat: type of clients

--- System/User.py
+++ System/User.py
@@ -6,6 +6,12 @@ class User:
         self.dif = dif
         self.data_nasc = data_nasc
         
+    def Criar_user(self, tipo, **kw):
+        super().__init__(**kw)
+        if tipo.upper() == 'PF':
+            PF(**kw)
+        elif tipo.upper() == 'PJ':
+            PJ(**kw)
 
 class PF(User):
     def __init__(self,**kw ):
