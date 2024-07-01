f4dc244ee2118a0904e6952814ec6ddd7d1a2ca2
Rhuan Pablo Monteiro Da Silva <rhuanpablomonteiro@gmail.com>
2024-07-01 16:26:57 -0300

    feat: new features(sacar,depositar,mostrar_extrato)

--- System/Account.py
+++ System/Account.py
@@ -1,15 +1,35 @@
 class conta:
     def __init__(self):
         self.saldo = 0.00
-        self.extrato = [] #TODO tuple for historico
+        self.extrato = [] 
 
-    #TODO def operation
+    def add_extrato(self,operacao,valor):
+        operacao_tupla = operacao,valor
+        self.extrato.append(operacao_tupla)
 
     def depositar(self,valor):
         print(self.saldo)
         self.saldo += valor
         print(self.saldo)
+        self.add_extrato('Deposito', valor)
+    
+    def sacar(self,valor):
+        print(self.saldo)
 
+        if valor > 0:
+            if valor <= self.saldo:
+                self.saldo -= valor
+
+            else:
+                print('sem saldo')
+
+                print(self.saldo)
+                self.add_extrato('Saque', valor)
+        else: print('valor invalido')
+
+    def mostrar_extrato(self):
+        print(self.extrato)
+        return self.extrato
         
         
 
--- System/testes.py
+++ System/testes.py
@@ -7,10 +7,12 @@ maria = Cliente('Maria', '987.654.321-00', '15/06/1990')
 pedro = Cliente('Pedro', '111.222.333-44', '10/03/1975')
 
 jao.Conta.depositar(100)
+jao.Conta.sacar(9)
 
+jao.Conta.mostrar_extrato()
 
-
-print(login('987.654.321-00').nome)
+maria.Conta.sacar(-100)
+maria.Conta.mostrar_extrato()
 
 
 #maria = Cliente(nome='Maria',dif='987.654.321-00', data_nasc='15/06/1990')
