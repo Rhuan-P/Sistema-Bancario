c144ac2b5e39d7f91f009d954b9ecaad99bdbba5
Rhuan Pablo Monteiro Da Silva <rhuanpablomonteiro@gmail.com>
2024-07-04 23:21:03 -0300

    Feat: acess

--- app/Modelo/banco.py
+++ app/Modelo/banco.py
@@ -6,9 +6,16 @@ class Banco:
     def __init__(self):
         self.clientes = []
 
+    def acessar(self, doc):
+       return next((cliente for cliente in self.clientes if cliente.doc == doc), None)
+       
+       
     def add_cliente(self, cliente):
         self.clientes.append(cliente)
 
     def ciar_cliente(self, nome, tipo, doc, data_nasc):
         cliente = Cliente(nome=nome, tipo=tipo, doc=doc, data_nasc=data_nasc)
         self.add_cliente(cliente)
+
+    def __str__(self):
+        return f'{self.clientes}'
