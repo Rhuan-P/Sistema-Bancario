1008ab3e799f6ddf9d485149c002fcdecedcc0c2
Rhuan Pablo Monteiro Da Silva <rhuanpablomonteiro@gmail.com>
2024-07-05 00:02:42 -0300

    Fix: name of module criar_cliente

--- app/Modelo/banco.py
+++ app/Modelo/banco.py
@@ -12,7 +12,7 @@ class Banco:
     def add_cliente(self, cliente):
         self.clientes.append(cliente)
 
-    def ciar_cliente(self, nome, tipo, doc, data_nasc):
+    def criar_cliente(self, nome, tipo, doc, data_nasc):
         cliente = Cliente(nome=nome, tipo=tipo, doc=doc, data_nasc=data_nasc)
         self.add_cliente(cliente)
 
