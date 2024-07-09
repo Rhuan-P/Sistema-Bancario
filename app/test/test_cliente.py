8484a8b8c58372e4b6f5d7b9afd3ee2d63a69f44
Rhuan Pablo Monteiro Da Silva <rhuanpablomonteiro@gmail.com>
2024-07-09 14:11:27 -0300

    Fix: unittest

--- app/test/test_cliente.py
+++ app/test/test_cliente.py
@@ -5,18 +5,18 @@ from app.Modelo.account import Conta
 class TestCliente(unittest.TestCase):
     def setUp(self):
         # Inicializa o Cliente para os testes
-        self.cliente = Cliente(nome="Fulano", tipo="PF", doc="123.456.789-00", data_nasc="01/01/1990")
+        self.cliente = Cliente(nome="Fulano", tipo="PF", doc="12345678900", data_nasc="01/01/1990")
 
     def test_criar_cliente(self):
         # Testando criação de cliente com dados válidos
         self.assertEqual(self.cliente.user.nome, "Fulano")
-        self.assertEqual(self.cliente.user.doc, "123.456.789-00")
+        self.assertEqual(self.cliente.user.doc, "12345678900")
         self.assertEqual(self.cliente.user.data_nasc, "01/01/1990")
         self.assertEqual(self.cliente.conta.saldo, 0.00)
 
     def test_validacao_tipo(self):
         # Testando validação de tipo de conta
-        self.assertRaises(ValueError, Cliente, nome="Ciclano", tipo="PJ", doc="987.654.321-00", data_nasc="02/02/1995")
+        self.assertRaises(ValueError, Cliente, nome="Ciclano", tipo="PP", doc="98765432100", data_nasc="02/02/1995")
 
     def test_acesso_conta(self):
         # Testando acesso ao objeto de conta
--- app/test/test_user.py
+++ app/test/test_user.py
@@ -4,17 +4,17 @@ from app.Modelo.user import PF, PJ
 class TestPF(unittest.TestCase):
     def test_criar_pf(self):
         # Testando criação de pessoa física (PF)
-        pf = PF(nome="João", doc="123.456.789-00", data_nasc="01/01/1990")
+        pf = PF(nome="João", doc="12345678900", data_nasc="01/01/1990")
         self.assertEqual(pf.nome, "João")
-        self.assertEqual(pf.doc, "123.456.789-00")
+        self.assertEqual(pf.doc, "12345678900")
         self.assertEqual(pf.data_nasc, "01/01/1990")
 
 class TestPJ(unittest.TestCase):
     def test_criar_pj(self):
         # Testando criação de pessoa jurídica (PJ)
-        pj = PJ(nome="Empresa XYZ", doc="98.765.432/0001-00", data_nasc="01/01/2000")
+        pj = PJ(nome="Empresa XYZ", doc="98765432000100", data_nasc="01/01/2000")
         self.assertEqual(pj.nome, "Empresa XYZ")
-        self.assertEqual(pj.doc, "98.765.432/0001-00")
+        self.assertEqual(pj.doc, "98765432000100")
         self.assertEqual(pj.data_nasc, "01/01/2000")
 
 if __name__ == "__main__":
